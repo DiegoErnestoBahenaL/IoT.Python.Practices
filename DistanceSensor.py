fb00c65921ee91349caa520bdfc72119ec5b9b0c
Diego Bahena <diegoeblopez@outlook.com>
2025-11-16 17:23:10 -0600

    Better formating for DistanceSensor

--- DistanceSensor.py
+++ DistanceSensor.py
@@ -4,5 +4,5 @@ from time import sleep
 
 ultrasonic = DistanceSensor(echo=17, trigger=4)
 while True:
-    print(ultrasonic.distance)
+    print( f" Objeto detectado a {ultrasonic.distance * 100:.2f} cm.")
     sleep(1)
