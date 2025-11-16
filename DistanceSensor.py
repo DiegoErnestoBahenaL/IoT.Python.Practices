257d989b4a7515c22945347a28debfa12d069abf
Diego Bahena <diegoeblopez@outlook.com>
2025-11-16 17:35:07 -0600

    Exception handling for KeyboardInterrupt in DistanceSensor

--- DistanceSensor.py
+++ DistanceSensor.py
@@ -1,8 +1,14 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-
 ultrasonic = DistanceSensor(echo=17, trigger=4)
-while True:
-    print( f" Objeto detectado a {ultrasonic.distance * 100:.2f} cm.")
-    sleep(1)
+
+try:
+
+    while True:
+
+        print( f" Objeto detectado a {ultrasonic.distance * 100:.2f} cm.")
+        sleep(1)
+
+except KeyboardInterrupt:
+    print("Program terminated by user.")
