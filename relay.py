49ffd891f53d8c773527fb4db342c2d4a813e75c
Diego Bahena <diegoeblopez@outlook.com>
2025-11-26 19:51:01 -0600

    added relay.py

---
+++ relay.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+# Set up GPIO pin numbering mode
+GPIO.setmode(GPIO.BCM)
+
+RELAY_PIN = 14
+
+GPIO.setup(RELAY_PIN, GPIO.OUT)
+
+
+try:
+
+    output = True
+
+    while True:
+
+        GPIO.output(RELAY_PIN, True)
+        output = not output
+        
+        time.sleep(1.5)
+
+except KeyboardInterrupt:
+    print("Programa termiinado por el usario.")
+finally:
+    GPIO.cleanup() # Clean up GPIO settings on exit        
